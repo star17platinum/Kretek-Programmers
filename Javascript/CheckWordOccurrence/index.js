5ae9d0358339f361f62a00a68f61a9b0b4d0df6e
09Farhanmahendra01 <githubfarhan@gmail.com>
2022-10-05 13:53:22 +0700

    add check word occurrence

---
+++ Javascript/CheckWordOccurrence/index.js
@@ -0,0 +1,23 @@
+const checkWordOccurrence = (str, isCaseSensitive = false) => {
+    if (typeof str !== 'string') {
+      throw new TypeError('The first param should be a string')
+    }
+  
+    if (typeof isCaseSensitive !== 'boolean') {
+      throw new TypeError('The second param should be a boolean')
+    }
+  
+    const modifiedStr = isCaseSensitive ? str.toLowerCase() : str
+  
+    return modifiedStr
+      .split(/\s+/) // remove all spaces and distribute all word in List
+      .reduce(
+        (occurrence, word) => {
+          occurrence[word] = occurrence[word] + 1 || 1
+          return occurrence
+        },
+        {}
+      )
+  }
+  
+  export { checkWordOccurrence }
