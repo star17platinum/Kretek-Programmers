8220af1fb4aaacf8bb3edad79843f147f4bdd24e
rizzturu13 <hasanrizky0405@gmail.com>
2022-10-14 09:40:45 +0700

    add PHP

---
+++ PHP/MVC/app/model/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+class user_model{
+    private $nama = 'Hasan Rizky';
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ PHP/MVC/app/view/home/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="h-100 p-5 text-bg-dark rounded-3 mt-3">
+        <h1>Selamat Datang Di Website Saya</h1>
+        <p class="lead">Halo, nama saya adalah <?=$data['nama']?></p>
+        <button class="btn btn-outline-light" type="button">Turu Dx</button>
+    </div>
+</div>
