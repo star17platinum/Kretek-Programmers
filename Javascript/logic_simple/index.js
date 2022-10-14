c8b1392439aed87019c55cbde1072378bc576643
RezaF15 <rezafitriadi666@gmail.com>
2022-10-14 10:02:47 +0700

    Logic-simple

---
+++ Javascript/logic_simple/index.js
@@ -0,0 +1,28 @@
+for (var x = 0; x <= 10; x++) {
+  for (var y = 0; y <= 10; y++) {
+    if (y == 5 || x == 10 || x > 9 || x < 4) {
+      document.write("*");
+    } else {
+      document.write("_");
+    }
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+for (var s = 0; s < 9; s++) {
+  for (var d = 0; d < 9; d++) {
+    if (s + d == 8) {
+      document.write(d * 2);
+    } else if (s == d) {
+      document.write(d * 2 + 1);
+    } else if (s == 4) {
+      document.write(d * 2);
+    } else if (d == 4) {
+      document.write(s * 2 + 1);
+    } else {
+      document.write("_");
+    }
+  }
+  document.write("<br>");
+}
