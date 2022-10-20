5efda820505359a29c9cdb7ae6d5e31949b1e4fe
Ismail Nur Alam <ismailnuralam@gmail.com>
2022-10-20 15:59:10 +0700

    Belajar Promise JS

---
+++ learn-promise/chainingPromise.js
@@ -0,0 +1,87 @@
+const state = {
+    stock: {
+        coffeeBeans: 250,
+        water: 1000,
+    },
+    isCoffeeMachineBusy: false,
+}
+ 
+const checkAvailability = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (!state.isCoffeeMachineBusy) {
+                resolve("Mesin kopi siap digunakan.");
+            } else {
+                reject("Maaf, mesin sedang sibuk.");
+            }
+        }, 1000);
+    });
+};
+ 
+const boilWater = () => {
+  return new Promise((resolve, reject) => {
+    console.log("Memanaskan air...");
+    setTimeout(() => {
+      resolve("Air panas sudah siap!");
+    }, 2000);
+  })
+}
+
+const grindCoffeeBeans = () => {
+  return new Promise((resolve, reject) => {
+    console.log("Menggiling biji kopi...");
+    setTimeout(() => {
+      resolve("Kopi sudah siap!");
+    }, 1000);
+  })
+}
+
+const checkStock = () => {
+    return new Promise((resolve, reject) => {
+        state.isCoffeeMachineBusy = true;
+        setTimeout(() => {
+            if (state.stock.coffeeBeans >= 16 && state.stock.water >= 250) {
+                resolve("Stok cukup. Bisa membuat kopi.");
+            } else {
+                reject("Stok tidak cukup!");
+            }
+        }, 1500);
+    });
+};
+ 
+const brewCoffee = () => {
+    console.log("Membuatkan kopi Anda...")
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Kopi sudah siap!")
+        }, 2000);
+    });
+};
+ 
+function makeEspresso() {
+    checkAvailability()
+        .then((value) => {
+            console.log(value);
+            return checkStock();
+        })
+        .then(value => {
+          console.log(value);
+          const promises = [boilWater(), grindCoffeeBeans()];
+
+          return Promise.all(promises);
+        })
+        .then((value) => {
+            console.log(value)
+            return brewCoffee();
+        })
+        .then(value => {
+            console.log(value);
+            state.isCoffeeMachineBusy = false;
+        })
+        .catch(rejectedReason => {
+            console.log(rejectedReason);
+            state.isCoffeeMachineBusy = false;
+        });
+}
+ 
+makeEspresso();
