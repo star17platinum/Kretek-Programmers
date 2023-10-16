d572373d362ae0308aece96a6a2f936c96901181
itzmail <ismailnuralam@gmail.com>
2023-10-16 14:12:01 +0700

    Add get period for dropdown

--- dart/calender_tools.dart
+++ dart/calender_tools.dart
@@ -3,6 +3,19 @@ import "dart:core"
 
 
 class CalenderTools {
+  static Map getPeriod({DateTime? dateTime}) {
+    DateTime now = DateTime.now();
+    DateFormat year = DateFormat('yyyy');
+    DateFormat month = DateFormat("MM");
+    DateFormat labelMonth = DateFormat('MMMM');
+
+    return {
+      "label":
+          "${labelMonth.format(dateTime ?? now)} ${year.format(dateTime ?? now)}",
+      "value": '${year.format(dateTime ?? now)}${month.format(dateTime ?? now)}'
+    };
+  }
+
   static Map getFirstLastDay({String? modFormat}) {
     DateTime now = DateTime.now();
     DateTime firstDayOfMonth = DateTime(now.year, now.month, 1);
